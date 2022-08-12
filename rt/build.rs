e896517992441455112455bc34077062813805b5
chris huang <youth7@163.com>
2022-08-12 18:33:51 +0800

    尝试简化代码

--- rt/build.rs
+++ rt/build.rs
@@ -1,14 +1,19 @@
-use std::{env, error::Error, fs::File, io::Write, path::PathBuf};
+// use std::{env, error::Error, fs::File, io::Write, path::PathBuf};
+
+use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // 构建脚本的输出目录
-    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
-    // 扩展库的搜索路径
-    println!("cargo:rustc-link-search={}", out_dir.display());
+    println!("");
+    // let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
+    // // 扩展库的搜索路径
+    // println!("cargo:rustc-link-search={}", out_dir.display());
+    println!("cargo:rustc-link-search={}", "D:\\workspace\\rust\\app\\rt");
+    
     // 将link.x复制到指定位置
-    File::create(out_dir.join("link.x"))?.write_all(include_bytes!("link.x"))?;
+    // File::create(out_dir.join("link.x"))?.write_all(include_bytes!("link.x"))?;
 
     Ok(())
 }
