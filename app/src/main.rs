fb23c229f927bc97bfdafad2339dff73a6b92422
chris huang <youth7@163.com>
2022-10-09 19:09:32 +0800

    初步完成第七章

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,34 +1,44 @@
 #![no_main]
 #![no_std]
 
+use cortex_m::interrupt;
 use cortex_m_semihosting::{
     debug,
     hio::{self, HostStream}//0.5.0之后改为使用HostStream结构体，原文中是使用HStdout
 };
 
-use log::{error, warn, Log};
+use log::{global_logger, log, GlobalLog};
 use rt::entry;
 
-struct Logger {
-    hstdout: HostStream,
-}
+struct Logger;
 
-impl Log for Logger {
-    type Error = ();
-    fn log(&mut self, address: u8) -> Result<(), ()> {
-        self.hstdout.write_all(&[address])
-    }
-}
+global_logger!(Logger);//将Logger注册为全局logger，这样在使用log!宏的时候就不再需要提供logger对象
 
 entry!(main);
 
 fn main() -> ! {
-    let hstdout = hio::hstdout().unwrap();
-    let mut logger = Logger { hstdout };
-    let _ = warn!(logger, "Hello, world!");
-    let _ = error!(logger, "Goodbye");
-    let _ = error!(logger, "你好呀");
-    let _ = warn!(logger, "是的师父！");
+    log!("Hello, world!");//更为简洁的日志API，不需要主动提供logger对象
+    log!("Goodbye");
     debug::exit(debug::EXIT_SUCCESS);
     loop {}
+}
+
+//全局logger的实现
+impl GlobalLog for Logger {
+    fn log(&self, address: u8) {
+        //interrupt::free作用是在一个无中断的上下文环境中执行函数，这是访问static mut类型变量的要求
+        //因为HSTDOUT是静态变量，只有做到这样才能保证内存安全。
+        //这种机制就是所谓的临界区（critical section）
+        interrupt::free(|_| unsafe {
+            static mut HSTDOUT: Option<HostStream> = None;
+
+            // 延迟初始化
+            if HSTDOUT.is_none() {
+                HSTDOUT = Some(hio::hstdout()?);
+            }
+            let hstdout = HSTDOUT.as_mut().unwrap();
+            hstdout.write_all(&[address])
+        })
+        .ok(); // 调用ok()意味着忽略错误并返回Option
+    }
 }
--- log/src/lib.rs
+++ log/src/lib.rs
@@ -1,28 +1,58 @@
 #![no_std]
 
+//关于Sync、Send的语义参考：https://www.zhihu.com/question/303273488/answer/2345814358
+pub trait GlobalLog: Sync {
+    /**
+    声明一个trait，全局的单例日志对象必须实现它，需要注意以下几点：
+    1，log方法只需要&self，不消耗所有权，因为它用的是单例的全局共享对象。
+    2，这里并没有像下面的Log trait那样定义一个关联的错误类型，这是为了简化细节。
+       将错误处理委托给用户，而不是由库指定错误处理规则并强制用户实现。
+    **/
+    fn log(&self, address: u8);
+}
+
 pub trait Log {
     type Error;
     fn log(&mut self, address: u8) -> Result<(), Self::Error>;
 }
 
-// 输出error等级的日志
 #[macro_export]
-macro_rules! error {
+macro_rules! log {
+    //该宏接受两种传参，第一种传参不需要提供日志对象，第二种传参需要提供日志对象
+
+    //第一种传参方式，此时会使用一个名为"LOGGER"的全局对象进行日志输出，它是一个定义在某处的全局对象
+    ($string:expr) => {
+        unsafe {
+            extern "Rust" {
+                //关于$crate请见：
+                //https://zjp-cn.github.io/tlborm/decl-macros/minutiae/hygiene.html?highlight=%24crate#unhygientic
+                //我们并不知道LOGGER的具体类型，但要求它必须实现了这里必须实现了GlobalLog，所以必须用trait object
+                static LOGGER: &'static dyn $crate::GlobalLog;
+            }
+
+            #[export_name = $string]
+            #[link_section = ".log"]
+            static SYMBOL: u8 = 0;
+
+            $crate::GlobalLog::log(LOGGER, &SYMBOL as *const u8 as usize as u8)
+        }
+    };
+
+    //第二种传参方式，需要用户自己提供日志对象进行输出，这是上一章的方式
     ($logger:expr, $string:expr) => {{
         #[export_name = $string]
-        #[link_section = ".log.error"] // 放置到.log.error这个节
+        #[link_section = ".log"]
         static SYMBOL: u8 = 0;
-        $crate::Log::log(&mut $logger, &SYMBOL as *const u8 as usize as u8)//最终都是调用log函数，只是放置的地方不一样
+
+        $crate::Log::log(&mut $logger, &SYMBOL as *const u8 as usize as u8)
     }};
 }
 
-// 输出warn等级的日志
+//提供一个宏，让用户注册单例的全局日志对象，并将符号名称定为"LOGGER"，这样正好和上面对应
 #[macro_export]
-macro_rules! warn {
-    ($logger:expr, $string:expr) => {{
-        #[export_name = $string]
-        #[link_section = ".log.warning"] // 放置到.log.warning这个节
-        static SYMBOL: u8 = 0;
-        $crate::Log::log(&mut $logger, &SYMBOL as *const u8 as usize as u8)//最终都是调用log函数，只是放置的地方不一样
-    }};
+macro_rules! global_logger {
+    ($logger:expr) => {
+        #[no_mangle]
+        pub static LOGGER: &dyn $crate::GlobalLog = &$logger;
+    };
 }
